7c25fedeed0e5135ccf0f1f361681487548166c8
LukasSteindl <LukasSteindl@outlook.com>
2021-03-10 09:40:06 +0100

    initial database created

---
+++ DWHBeispielDB/DWHBeispielDB/dbo/Tables/Table_1.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[Table_1] (
+    [tset] NCHAR (10) NULL
+);
+
---
+++ DWHBeispielDB/DWHBeispielDB/dbo/Tables/Test.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Test] (
+    [id]            INT               IDENTITY (1, 1) NOT NULL,
+    [x]             CHAR (10)         NULL,
+    [Beruf]         NCHAR (10) SPARSE NULL,
+    [Beruf2]         NCHAR (10) SPARSE NULL,
+    [Vorerkrankung] NCHAR (10) SPARSE NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [NonClusteredIndex-20210302-102542]
+    ON [dbo].[Test]([Beruf] ASC) WHERE ([Beruf] IS NOT NULL);
+
